e664d47b176b7792992c8b459ef3b0e03303bb6b
Eddy A. Murga Barillas <30765150+EmBarillasA94@users.noreply.github.com>
2020-06-12 04:01:13 -0600

    Agregando Clase transaccion

---
+++ EDD_VAC_JUN_PROYECTO1_201503741/Transaccion.h
@@ -0,0 +1,48 @@
+#ifndef TRANSACCION_H
+#define TRANSACCION_H
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Transaccion
+{
+public:
+	Transaccion(string id_Activo_, string usuario_, string depto_, string empresa_, string fecha_, string tiempo_) {
+		//generar id automaticamente
+		this->id_Transaccion = GenerarId(15);
+		this->id_Activo = id_Activo_;
+		this->usuario = usuario_;
+		this->depto = depto_;
+		this->empresa = empresa_;
+		this->fecha = fecha_;
+		this->tiempo = tiempo_;
+	}
+	string GenerarId(int length);
+
+private:
+	string id_Transaccion;//se genera automaticamente
+	string id_Activo;
+	string usuario;
+	string depto;
+	string empresa;
+	string fecha;
+	string tiempo;
+};
+
+string Transaccion::GenerarId(int tamanio)
+{
+	auto randchar = []() -> char
+	{
+		const char charset[] =
+			"0123456789"
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+			"abcdefghijklmnopqrstuvwxyz";
+		const size_t max_index = (sizeof(charset) - 1);
+		return charset[rand() % max_index];
+	};
+	string id(tamanio, 0);
+	std::generate_n(id.begin(), tamanio, randchar);
+	return id;
+}
+#endif // !TRANSACCION_H
